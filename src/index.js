cb54904b244cb4582de18742f3b6579e8fb598de
Laurynas Karvelis <hello@explosive-brains.com>
2026-08-24 12:54:27 +0300

    Remove redundant js files

--- src/index.js
+++
@@ -1,7 +0,0 @@
-export { DEFAULT_CACHE_OPTIONS } from "./cache";
-export { AUTHORITY_PUBLIC_KEY, PLAN, PLAN_NAME, PROTOCOL_VERSION, PUBLISHER_HEADER, TOKEN_HEADER, TOKEN_HEADER_LOWERCASE, } from "./constants";
-export { canonicalHostname } from "./hostname";
-export { createPublisher } from "./publisher";
-export { encodePublisherHeader, parsePublisherHeader } from "./publisher-header";
-export { REJECTED } from "./rejection";
-export { TOKEN_BYTES, TOKEN_CHARACTERS } from "./token";
